a208dc45a5c084cbf9611d971767d8066ebc829d
Dave Chao <dave.chao@silkrode.com.tw>
2020-06-24 09:55:59 +0800

    refactor coder

--- lib/model/config.dart
+++ lib/model/config.dart
@@ -1,13 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class Config {
   final String flavor;
   final GraphQLClient graphQLClient;
-  final String apiBaseUrl;
+  final Dio dio;
 
   Config({
     this.flavor,
     this.graphQLClient,
-    this.apiBaseUrl,
+    this.dio,
   });
 }
--- lib/model/repository/account_repository.dart
+++ lib/model/repository/account_repository.dart
@@ -1,28 +1,13 @@
-import 'dart:io';
-
 import 'package:dio/dio.dart';
 import 'package:fimber/fimber.dart';
 import 'package:podcastapp/model/repository/vo/login_item.dart';
 import 'package:podcastapp/model/repository/vo/login_request.dart';
 
 class AccountRepository {
-  String _baseUrl;
   Dio _dio;
 
-  AccountRepository(String baseUrl) {
-    _baseUrl = baseUrl;
-    _dio = _baseDio();
-  }
-
-  Dio _baseDio() {
-    Dio dio = Dio(new BaseOptions(
-      baseUrl: _baseUrl,
-      connectTimeout: 5000,
-      receiveTimeout: 100000,
-      contentType: ContentType.json.toString(),
-      responseType: ResponseType.json,
-    ));
-    return dio;
+  AccountRepository(Dio dio) {
+    _dio = dio;
   }
 
   Future<LoginItem> login(LoginRequest request) async {
